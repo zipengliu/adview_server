3e67131a7db524ec17aee5c1e6f953111aa47421
Zipeng Liu <zipeng@cs.ubc.ca>
2017-06-26 16:56:37 -0700

    Add requirements; Add API to build consensus tree

--- visphy_server.py
+++ visphy_server.py
@@ -1,8 +1,15 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_compress import Compress
 from flask_cors import CORS
-import sys
 from pymongo import MongoClient
+from dendropy import Tree, TaxonNamespace
+import sys, os, subprocess
+from datetime import datetime
+import uuid
+
+COMPRESS_LEVEL = 6
+TEMP_CONSENSUS_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'temp_consensus')
+CONSENSUS_PARALLIZATION = '4'
 
 
 class Connection():
@@ -86,6 +93,105 @@ def get_dataset(input_group_id):
     })
 
     return jsonify(data)
+
+
+def read_tree_from_consensus_report(filepath):
+    newick_string = None
+    with open(filepath, 'r') as f:
+        mark = False
+        for line in f:
+            if mark:
+                newick_string = line.strip()
+                break
+            if line.startswith('BEGIN TREES'):
+                mark = True
+    newick_string = newick_string[newick_string.find('('):]
+    return newick_string
+
+
+def parse_tree(tree, entities):
+    tree.ladderize()
+
+    branches = {}
+    no = 0
+    for node in tree.levelorder_node_iter():
+        assert node.is_leaf() or node.num_child_nodes() == 2
+        node.bid = 'b' + str(no)
+        no += 1
+
+    present_entities = {}
+    for node in tree.levelorder_node_iter():
+        if node.is_leaf():
+            label = node.taxon.label.replace(' ', '_')
+            branches[node.bid] = {
+                'entity': entities[label],
+                # 'support': node.support,
+                'length': node.edge_length
+            }
+            present_entities[entities[label]] = True
+        else:
+            branches[node.bid] = {
+                'left': node.child_nodes()[0].bid,
+                'right': node.child_nodes()[1].bid,
+                'support': float(node.label),
+                'length': node.edge_length
+            }
+
+    data = {
+        'name': 'Consensus tree',
+        'branches': branches,
+        'entities': present_entities.keys(),
+        'rfDistance': {},       # Cost is too high?
+        'rootBranch': 'b0',
+        'tid': 'consensus-' + str(uuid.uuid4())
+    }
+
+    return data
+
+
+@app.route('/consensus', methods=['POST'])
+def get_consensus():
+    input_group_id = int(request.form['inputGroupId'])
+    tids = str(request.form['trees']).split(',')
+    print 'Creating consensus tree for ', tids, 'in dataset', input_group_id
+
+    # Retrieve the tree data (newick string) from the DB
+    tree_cursor = connection.tree.find({'inputGroupId': input_group_id}, projection={'tid': True, 'newickString': True})
+    trees = {tid: True for tid in tids}
+    remain = len(tids)
+    tree_str = []
+    for d in tree_cursor:
+        if d['tid'] in trees:
+            tree_str.append(d['newickString'])
+            remain -= 1
+            if remain == 0:
+                break
+    assert remain == 0, 'Failed to find newick string for some trees'
+
+    # Write the strings to a file
+    tree_filename = datetime.now().strftime('%m%d%H%M%S%f') + '.tre'
+    tree_path = os.path.join(TEMP_CONSENSUS_PATH, tree_filename)
+    with open(tree_path, 'w') as f:
+        f.write('\n'.join(tree_str))
+
+    # Invoke sumtrees.py
+    consensus_path = os.path.join(TEMP_CONSENSUS_PATH, tree_filename + '.consensus')
+    try:
+        subprocess.check_call(['sumtrees.py', '--output-tree-filepath=' + consensus_path, '--suppress-annotations',
+                               '--multiprocessing=' + CONSENSUS_PARALLIZATION, tree_path],
+                              stdout=open(os.devnull, 'w'), stderr=subprocess.STDOUT)
+    except subprocess.CalledProcessError:
+        # TODO
+        pass
+
+    # Read the entity mapping from DB
+    entity_cursor = connection.entity.find({'inputGroupId': input_group_id}, projection={'eid': True, 'name': True})
+    entities = {str(e['name']): str(e['eid']) for e in entity_cursor}
+
+    # Read the consensus tree and parse
+    newick_string = read_tree_from_consensus_report(consensus_path)
+    tree = Tree.get(data=newick_string, schema='newick')
+    return jsonify(parse_tree(tree, entities))
 
 if __name__ == '__main__':
     app.run(port=33333)
